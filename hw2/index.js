23a9ea16a77d3a2b516579a0e2c21bf1438370a3
Natalex17 <natalex.iva@bk>
2024-01-05 20:03:30 +0300

    4 задание в дз выполнила

--- hw2/index.js
+++ hw2/index.js
@@ -8,3 +8,16 @@ alert (ifpone)
 let userName = "BrendanEich" // Имя разработчика javaScript
 alert (userName)
 
+let ap = Number (10);
+let up = Number (2);
+    let result = ap + up
+    alert (result);
+
+    let subtraction = ap - up;
+    alert (subtraction);
+
+    let multiplication = ap * up;
+    alert (multiplication)
+    
+    let division = ap / up
+    alert (division)
