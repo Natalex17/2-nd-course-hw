92ed30451f77b0367e48dc88634aad8ae36b9871
Natalex17 <natalex.iva@bk>
2024-01-20 23:34:59 +0300

    выполнила 1-7 задание , думаю над 8

--- hw6/script6.js
+++ hw6/script6.js
@@ -31,11 +31,50 @@ console.log (numeral)
 //Задание 4
 //С помощью вложенных циклов создайте многомерный массив вида: 
 //[[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+let arr = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
+
+for (let i = 0; i < 3; i++) {
+	arr[i] = []; // создаем подмассив
+	
+	for (let j = 0; j < 3; j++) {
+		arr[i].push(j + 1); // заполняем подмассив числами
+	}
+}
+
+console.log(arr);
 
 
-//Задание 5
-//Дан массив: 
-//[1, 1, 1]
+// 5 
+let numb = [1, 1, 1]
+numb.push(2, 2, 2);
+console.log(numb);
+
+//6
+let nuM = [9, 8, 7, 'a', 6, 5];
+delete nuM [3]; // удаляем `а`
+nuM = nuM.sort (); // сортируем
+console.log (nuM);
 
 
-//Добавьте в конец массива значения 2, 2, 2.
+
+//Задание 7
+let rondom = [9, 8, 7, 6, 5];
+if (rondom.includes(Number (prompt(`Угадай число`)))) {
+    alert(`Угадал`)
+} else {
+    alert(`Не угадал`)
+
+}
+
+// 8
+//let word ='abcdef'
+//word = word.split();
+//console.log(word);
+
+let  word ='abcdef'
+for (let item of word) {
+	console.log(item);
+}
+
+//Необходимо, чтобы программа вывела в консоль 
+//'fedcba'
