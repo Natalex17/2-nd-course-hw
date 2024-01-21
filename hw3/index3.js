fe429ac85e0b057af33609c9861e1fa1db9637a2
Natalex17 <natalex.iva@bk>
2024-01-21 18:52:15 +0300

    эннаяое осправление

--- hw3/index3.js
+++ hw3/index3.js
@@ -38,25 +38,25 @@ switch (monthNumber) {
     case 1:
     case 2:
     case 12:
-        alert (`Зима`);
-        
-            case 3:
-            case 4:
-            case 5:
-            alert (`Весна`);
-           
-                case 6:
-                case 7:
-                case 8:
-                alert (`Лето`);
-                
-                    case 9:
-                    case 10:
-                    case 11:
-                    alert (`Осень`);
-                    break;
+        alert(`Зима`);
+        break;
+    case 3:
+    case 4:
+    case 5:
+        alert(`Весна`);
+        break;   
+    case 6:
+    case 7:
+    case 8:
+        alert(`Лето`);
+        break;        
+    case 9:
+    case 10:
+    case 11:
+        alert(`Осень`);
+        break;
                         
-default: alert (`Неизвестный месяц`)
+default: alert(`Неизвестный месяц`)
 
     }
     
