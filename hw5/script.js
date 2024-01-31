6461a42ba2f2e5153eb1852dab9ae70c151f0587
Natalex17 <natalex.iva@bk>
2024-01-31 19:22:01 +0300

    доработать

--- hw5/script.js
+++ hw5/script.js
@@ -75,7 +75,7 @@ function getMin (a, b) {
   const circle1 = {
       radius: 5,    //задаем радиус круга
       getArea: getArea  //ссылка на функцию вычисления Р
-      getPerimeter, //ссылка на вычисления периметра
+     // getPerimeter, //ссылка на вычисления периметра
   };
   
   const circle2 = {
