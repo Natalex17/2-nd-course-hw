de2841599c5ef844b76fd3bda3b3b848a1509926
Natalex17 <natalex.iva@bk>
2024-02-02 15:02:20 +0300

    Задачи по 7ой домашке выполнила

--- hw7/script7.js
+++ hw7/script7.js
@@ -25,7 +25,7 @@ console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр']
 
  function getRandomInt(min, max) {
      return Math.round(32.58884); //до ближ. целого 33 почему NAN?
-    }  console.log (getRandomInt ());
+    }  console.log (getRandomInt());
 
   function getRandomInt1 (max) {
     return Math.ceil(32.58884);  // до большего целого 33
@@ -44,8 +44,8 @@ const numbers = [52, 53, 49, 77, 21, 32];
 const min = Math.min(...numbers);
 const max = Math.max(...numbers);
 
-console.log(`Наименьшее число: ${min}`);
-console.log(`Наибольшее число: ${max}`);
+console.log(`Наименьшее число: ${min}`); //21
+console.log(`Наибольшее число: ${max}`); //77
 
 //5 
 
@@ -70,8 +70,43 @@ console.log(getRandomInt(5, 10));
 
 //7 Напишите функцию, которая на вход принимает 2 целых числа, а в качестве результата возвращает случайное целое число в этом диапазоне.
 
+//НАДО ПОДУМАТЬ
+
+function getRandomInt(min, max) {
+ return Math.round(Math.random() * (max - min) + min); 
+} console.log(getRandomInt(1, 5))
 
 
+  // 8 
+
+  let currentDate = new Date();
+  console.log(currentDate);  //ВОЗМОЖНО РАСМСМОТРЕТЬ ДРУГОЙ СПОСОБ
+
+
+  
+//9 Работает
+
+let currentDate1 = new Date();
+currentDate.setDate(currentDate.getDate() + 73);
+
+console.log(currentDate); //... апрель 2024
+
+//10 
+function ruDateTime() {
+    const myDate = new Date();
+    const months = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
+    const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    let ruDateTime = "Дата:" + myDate.getDate() + " " + months[myDate.getMonth()] + " " + myDate.getFullYear() + " " + days[myDate.getDay()] + "." + "Время:" + myDate.toLocaleTimeString();
+    return ruDateTime;
+}
+ruDateTime();
+console.log(ruDateTime())
+
+
+
+
+  
+  
 
 
 
